99785bdf601ed1cc9a03fdcc91833adbade2bcd2
feudal <calmis87@gmail.com>
2021-09-17 01:17:47 +0300

    error in ProfileForm.js fixed

--- src/components/Profile/ProfileForm/ProfileForm.js
+++ src/components/Profile/ProfileForm/ProfileForm.js
@@ -7,23 +7,23 @@ const ProfileForm = (props) => {
     return (
         <form className={classes.form}>
             <label className={classes.label}>
-                <span>Email</span>
+                <span className={classes.span}>Email</span>
                 <input className={[classes.input, classes['input--lock']].join(' ')} type="email" placeholder='completati' value={profile.email} readOnly/>
             </label>
             <label className={classes.label}>
-                <span>Nume prenume</span>
+                <span className={classes.span}>Nume prenume</span>
                 <input className={classes.input} type="text" placeholder={profile.name ? profile.name : 'completati'} />
             </label>
             <label className={classes.label}>
-                <span>Telefon</span>
+                <span className={classes.span}>Telefon</span>
                 <input className={classes.input} type="text" placeholder={profile.mobile ? profile.mobile : 'completati'} />
             </label>
             <label className={classes.label}>
-                <span>Data de naștere</span>
+                <span className={classes.span}>Data de naștere</span>
                 <input className={classes.input} type="date" value={profile.dob} />
             </label>
             <label className={classes.label}>
-                <span>Despre mine</span>
+                <span className={classes.span}>Despre mine</span>
                 <textarea className={classes.textarea} value={profile.text}/>
             </label>
             <input className={classes.button} type="submit" value='Applica schimbarile'/>
