75cb98a9e2f9d5f069bca9a21de08469c15d0a45
feudal <calmis87@gmail.com>
2021-09-16 16:53:37 +0300

    editBtn link added

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './ProfileInfo.module.css';
+import {Link} from "react-router-dom";
 
 
 const ProfileInfo = (props) => {
     const profile = props.profileInfo;
     return (
-        <div>
+        <div className={classes.container}>
+            <Link className={classes['edit-btn']} to='/edit-profile'/>
             <h3 className={classes['main-title']}>Despre mine</h3>
             <p className={classes.text}>{profile.text}</p>
             <div className={classes.group}>
