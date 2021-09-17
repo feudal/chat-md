02c1c79fe06d0ae086382a0096cb01f6a8bda2b7
feudal <calmis87@gmail.com>
2021-09-17 03:41:15 +0300

    CloseBtn component

---
+++ src/components/UI/CloseBtn/CloseBtn.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './CloseBtn.module.css';
+
+const CloseBtn = () => {
+    return <button className={classes['close-btn']}/>
+};
+
+export default CloseBtn;
