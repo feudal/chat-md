612beb112e26b7dd2313989c8c4703a77cc9f7b0
feudal <calmis87@gmail.com>
2021-09-19 13:54:25 +0300

    component Loader.

---
+++ src/components/decoration/Loader/Loader.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import classes from './Loader.module.css';
+
+const Loader = () => {
+    return (
+        <>
+            <div className={classes.loader}>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+            </div>
+            <div className={classes['second--loader']}>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+                <div className={classes['oblique-line']}/>
+            </div>
+        </>
+    );
+};
+
+export default Loader;
