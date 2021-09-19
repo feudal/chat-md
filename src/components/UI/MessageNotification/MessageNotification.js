6011ab1391eec4ffbd53e1b31db7b3468e37fff4
feudal <calmis87@gmail.com>
2021-09-19 14:07:12 +0300

    Added loader in MessageNotification.

--- src/components/UI/MessageNotification/MessageNotification.js
+++ src/components/UI/MessageNotification/MessageNotification.js
@@ -4,6 +4,7 @@ import CloseBtn from "../CloseBtn/CloseBtn";
 import {useDispatch, useSelector} from "react-redux";
 import {uiActions} from "../../../store/ui";
 import classNames from "classnames/bind";
+import Loader from "../../decoration/Loader/Loader";
 
 const MessageNotification = () => {
     const dispatch = useDispatch();
@@ -21,10 +22,12 @@ const MessageNotification = () => {
         {'notification--closed': !showMessageNotification},
         {'notification--error': messageNotificationState.type === 'Error'},
         {'notification--success': messageNotificationState.type === 'Success'},
-        {'notification--info': messageNotificationState.type === 'Info'});
+        {'notification--info': messageNotificationState.type === 'Info'},
+        {'notification--loading': messageNotificationState.type === 'Loading'});
 
     return (
         <div className={classForMessageNotification}>
+            {(messageNotificationState.type === 'Loading') && <Loader/>}
             <CloseBtn close={toggleMessageNotificationHandler}/>
             <span className={classes.text}>{messageNotificationState.type}</span>
             <span className={classes.text}>{messageNotificationState.message}</span>
