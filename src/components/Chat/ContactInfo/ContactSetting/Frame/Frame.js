5996b528b1a86be9ed5eb68901cddf7b875120e5
feudal <calmis87@gmail.com>
2021-09-25 14:48:13 +0300

    Styling frame component.

--- src/components/Chat/ContactInfo/ContactSetting/Frame/Frame.js
+++ src/components/Chat/ContactInfo/ContactSetting/Frame/Frame.js
@@ -2,10 +2,12 @@ import React from 'react';
 import classes from './Frame.module.css';
 
 const Frame = (props) => {
+    const emailLink = 'mailto:'+ props.email;
     return (
         <div className={classes.frame}>
             <div className={classes.img}>img</div>
             <h3 className={classes.name}>{props.name}</h3>
+            <a className={classes.contact} href={emailLink}>{props.email}</a>
         </div>
     );
 };
